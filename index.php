4b9a8f57d9f8e6e2d49c0e0e1d404e564747c75a
leonmanu <leonmanu@gmail.com>
2015-08-30 19:41:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
-<?php
-echo 'Odio PHP';
-?>
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>The Wall</title>
+			<link rel="stylesheet" href="fonts/style.css">
+			<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+			<link rel="icon" type="image/png" href="img/muroBlanco.png">
+		</head>
+		<body>
+			<div class="container-fluid">
+				<h1 id="titulo">The Wall</h1>
+			</div>
+			<img id="muro" src="img/muroBlanco.jpg">
+			<div class="container-fluid alert alert-info">
+				<div class="marginAuto form-group form-inline">
+					<label for="focusedInput">Usuario</label>
+				    <input class="form-control" id="focusedInput" type="text">
+				    <label for="focusedInput">Contraseña</label>
+				    <input class="form-control" id="focusedInput" type="password">
+				    <button type="button" class="btn btn-primary active">Iniciar Sesión</button>
+			    </div>
+			</div>
+			<div class="conteinerCentrado panel-body">
+				<div class="container-fluid col-sm-4">
+					<h3>Regístrese</h3>
+					<div class="form-group">
+						<label for="focusedInput">Nombre</label>
+					    <input class="form-control" id="focusedInput" type="text">
+					    <label for="focusedInput">Apellido</label>
+					    <input class="form-control" id="focusedInput" type="text">
+					    <label for="focusedInput">Nombre de Usuario</label>
+					    <input class="form-control" id="focusedInput" type="text">
+					    <label for="focusedInput">Correo electrónico</label>
+					    <input class="form-control" id="focusedInput" type="text">
+					    <label for="focusedInput">Contraseña</label>
+					    <input class="form-control" id="focusedInput" type="password">
+					    <label for="focusedInput">Confirme la contraseña</label>
+					    <input class="form-control" id="focusedInput" type="password">
+				    </div>
+				    <div class="form-group">
+					    <button type="button" class="btn btn-primary active">Registrarse</button>
+				    </div>
+			    </div>
+			</div>
+
+			<div class="panel-footer alert-info">
+				<p class="text-center">Universidad Nacional de La Matanza</p>
+				<div class="text-right">
+					<p>Integrantes:</p>
+					<p>García, Matías</p>
+					<p>León, Manuel</p>
+					<p>Magnoni, Nicolás</p>
+					<p>Ramírez, Federico</p>
+					<p>Villaroel, Jonatan</p>
+				</div>
+			</div>
+
+			<script src="js/jquery-1.11.1.min.js"></script>
+			<script src="js/main.js"></script>
+			<link rel="stylesheet" href="bootstrap/css/bootstrapLimpio.min.css">
+			<link rel="stylesheet" href="css/main.css">
+			<script src="bootstrap/js/jquery.min.js"></script>
+			<script src="bootstrap/js/bootstrap.min.js"></script>
+
+			<!-- Add mousewheel plugin (this is optional) -->
+			<script type="text/javascript" src="fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+			<!-- Add fancyBox -->
+			<link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+			<script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>	
+		</body>
+	</html>
